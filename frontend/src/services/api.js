5f84f45ea0b74adef97b2290c6ecf6268647b807
Fathimathul Mizba <mizba@kiebot.com>
2026-03-24 13:53:33 +0530

    updated production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://ai-flow-backend-bblo.onrender.com"
 });
 
 export const askAI = (prompt) => API.post("/ask-ai", { prompt });
