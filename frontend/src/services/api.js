4dce1af999f266f414d7b756f973ed8e12425d8e
Fathimathul Mizba <mizba@kiebot.com>
2026-03-24 14:55:46 +0530

    final api config with env support

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-flow-backend-bblo.onrender.com/api"
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://ai-flow-backend-bblo.onrender.com/api" // live backend
+      : "http://localhost:5000/api", //  local backend
 });
 
 export const askAI = (prompt) => API.post("/ask-ai", { prompt });
+
 export const saveData = (prompt, response) =>
   API.post("/save", { prompt, response });
